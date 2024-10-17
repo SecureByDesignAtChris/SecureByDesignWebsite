be1ab05e9c54165359e019bc70fcd8698aeb2295
SecureByDesignAtChris <securebydesign@outlook.dk>
2024-10-17 18:03:21 +0000

    fix build more

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -8,8 +8,6 @@ export default function Button({ text, className, href, newTab }) {
         className="main-button"
         href={href}
         target={newTab && "_blank"}
-        onMouseEnter={(event) => onMouseEnter(event)}
-        onMouseOut={(event) => onMouseOut(event)}
       >
         {text}
       </a>
